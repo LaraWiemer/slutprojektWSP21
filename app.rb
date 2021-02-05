261add7d92d0ac2b039e64263acae3e8ceefdc21
LaraWiemer <60173230+LaraWiemer@users.noreply.github.com>
2021-02-05 14:22:20 +0100

    2021-02-05

--- app.rb
+++ app.rb
@@ -1,1 +1,22 @@
-require slim
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+enable :sessions
+
+
+
+
+
+
+
+
+get('/ad/new') do
+    slim(:"ads/create")
+end
+
+post("/ad/new") do
+
+end
+
+
