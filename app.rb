1f956036d3054af021b825c5e153a722270354ee
LaraWiemer <60173230+LaraWiemer@users.noreply.github.com>
2021-04-23 14:45:25 +0200

    2021-04-23

--- app.rb
+++ app.rb
@@ -3,27 +3,77 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+require 'byebug'
 enable :sessions
+require 'fileutils'
+require 'securerandom'
 
 
 
 get('/') do
-    slim(:index)
+    #user_id = session[:user_id].to_i
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM advertisements")
+    slim(:index, locals:{advertisements:result})
 end
 
-get('/ad/new') do
+get('/ads/new') do
     slim(:"ads/create")
 end
 
-post("/ad/new") do
-    ad_name = params[:new_ad_name]
+post("/ads/new") do
+    ad_name = params[:ad_name]
     description = params[:description]
-    img_url = params[:img]
     price = params[:price]
-    path = File.join("/img/uploaded_pictures/",params[:file][:filename])    
-    File.write(path,File.read(params[:file][:tempfile]))
-    add_ad(ad_name, description, img_url, price)
+    user_id = session[:user_id]
+    original_filename = params[:file][:filename]
+    file_ending = search_file_ending(original_filename)
+    filename = SecureRandom.uuid + file_ending #gör så att den kollar ändelse; params[:file][:filename]
+    save_path = File.join("./public/img/uploaded_pictures/", filename)
+    db_path = File.join("/img/uploaded_pictures/", filename)
+    #File.write(save_path,File.read(params[:file][:tempfile]))
+    FileUtils.cp(params[:file][:tempfile], "./public/img/uploaded_pictures/#{filename}")
+    add_ad(ad_name, description, db_path, price, user_id)
     redirect("/")
 end
 
+get("/register") do
+    slim(:register_user)
+end
 
+post("/register") do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    phone_number = params[:phone_number]
+    if password = password_confirm
+        register_user(username, password, phone_number)
+        redirect("/")
+    else
+        redirect("/register_error")
+    end
+end
+
+get("/login") do
+    slim(:login)
+end
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    login_result = login(username, password)
+    if login_result == true
+        redirect('/')
+    else
+        redirect('/login_error')
+    end
+end
+
+get("/login_error") do
+    slim(:login_error)
+end
+
+get("/register_error") do
+    slim(:register_error)
+end
--- model.rb
+++ model.rb
@@ -2,12 +2,39 @@
 
 
 
-def add_ad(ad_name, description, img_url, price)
+def add_ad(ad_name, description, db_path, price, user_id)
     db = SQLite3::Database.new('db/handel.db')
     db.results_as_hash = true
-    db.execute("INSERT INTO advertisements(name, description, img_url, price) VALUES (?, ?, ?, ?)", ad_name, description, img_url, price)
+    db.execute("INSERT INTO advertisements(name, description, img_path, price) VALUES (?, ?, ?, ?)", ad_name, description, db_path, price)
+    current_ad = db.execute("SELECT * FROM advertisements WHERE name = ? AND description = ? ", ad_name, description).first
+    ad_id = current_ad["ad_id"]
+    db.execute("INSERT INTO user_to_ad(user_id, ad_id) VALUES (?,?)", user_id, ad_id)
 end
 
-def login_user(username)
-    
+def register_user(username, password, phone_number)
+    pwdigest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new('db/handel.db')
+    db.execute("INSERT INTO users(username, pwdigest, phone_number) VALUES (?, ?, ? , ?)", username, pwdigest, phone_number)
 end
+
+def search_file_ending(original_filename)
+    array = original_filename.split(".")
+    result = "." + array[1]
+    return result
+end
+
+def login(username, password)
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+    if BCrypt::Password.new(pwdigest) == password
+        session[:user_id] = id
+        return true
+      else
+        return false
+      end
+
+end
+
