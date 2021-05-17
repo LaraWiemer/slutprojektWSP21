9d9d5b4916df57d54567fbf31c89172d972ba5b6
Lara Wiemer <lara.wiemer@elev.ga.ntig.se>
2021-05-17 15:12:30 +0200

    17-05-21

--- app.rb
+++ app.rb
@@ -8,28 +8,54 @@ enable :sessions
 require 'fileutils'
 require 'securerandom'
 
+include Model
 
+before all_of("/ads/new", "/ads/edit", "/ads/:id/update", "/ads/:id/delete", "/ads/:id/update", "/ads/:id/update_picture", "/users/profile") do
+    ad_id = params[:ad_id]
+    if session[:user_id] == nil
+        redirect("/error")
+    end
+    if ad_id != nil
+        result = check_user_id_ad(ad_id)
+        if session[:user_id] == result["ad_id"]
+            redirect("/error")
+        end
+    end
+end
 
-get('/') do
-    #user_id = session[:user_id].to_i
-    db = SQLite3::Database.new('db/handel.db')
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM advertisements")
-    slim(:index, locals:{advertisements:result})
+before all_of("/login", "/register") do
+    if session[:sleep] == true
+        redirect("/")
+    end
 end
 
 
+#Visa startsidan
+#@see Model#get_ads
+
+get('/') do 
+    advertisements = get_ads()
+    slim(:index, locals:{advertisements:advertisements})
+end
 
 
-#Ads
+#Shows the form that allows the user to create an ad
+#@see Model#get_categories
 
 get('/ads/new') do
-    if login_check() == false
-        redirect("/")
-    end
     categories = get_categories()
     slim(:"/ads/create", locals:{categories:categories})
 end
+
+
+#Creates a new ad and redirects to /
+#@param [string] :ad_name, the name of the advertisement
+#@param [string] :description, the description of the advertisement
+#@param [string] :category, the category of the advertisement
+#@param [string] :price, the price of the advertisement
+#@param [string] :file, the image file of the advertisement
+#@param [string] :filename, the image files name of the advertisement
+#@see Model#add_ad
 
 post("/ads/new") do
     ad_name = params[:ad_name]
@@ -47,10 +73,14 @@ post("/ads/new") do
     redirect("/")
 end
 
+
+#Views the form that can be used to edit an ad
+#@param [string] :ad_id, the id of the ad being edited
+#@see Model#ad_content
+#@see Model#ad_to_category
+#@see Model#get_categories
+
 get("/ads/edit") do
-    if login_check() == false
-        redirect("/")
-    end
     ad_id = params[:ad_id].to_i
     user_id = session[:user_id].to_i
     ad_content = ad_content(ad_id)
@@ -59,8 +89,17 @@ get("/ads/edit") do
     slim(:"/ads/edit", locals:{advertisements:ad_content, categories:categories, category_id:category_id})
 end
 
+
+#Updates the chosen ad and redirects to /users/profile
+#@param [string] :id, the id of the ad being updated
+#@param [string] :ad_name, the new name of the ad being updated
+#@param [string] :description, the new description of the ad being updated
+#@param [string] :category_id, the id of the new category of the ad being updated
+#@param [string] :price, the new price of the ad being updated
+#@see Model#update_ad
+
 post("/ads/:id/update") do
-    id = params[:ad_id].to_i
+    id = params[:id].to_i
     ad_name = params[:ad_name]
     description = params[:description]
     category_id = params[:category]
@@ -70,6 +109,13 @@ post("/ads/:id/update") do
     redirect("/users/profile")
 end
 
+
+#Updates the image of the selected ad and redirects to /users/profile
+#@param [string] :ad_id, the id of the ad being updated
+#@param [string] :file, the new image file of the ad being updated
+#@param [string] :filename, the new name of the image of the ad being updated
+#@see Model#search_file_ending
+#@see Model#update_ad_picture
 
 post("/ads/:id/update_picture") do
     id = params[:ad_id].to_i
@@ -83,6 +129,11 @@ post("/ads/:id/update_picture") do
     redirect('/users/profile')
 end
 
+
+#Deletes the selected ad and redirects to /users/profile
+#@param [string] :id, the id of the ad being deleted
+#@see Model#delete_ad
+
 post('/ads/:id/delete') do
     id = params[:ad_id].to_i
     user_id = session[:user_id]
@@ -91,12 +142,19 @@ post('/ads/:id/delete') do
 end
 
 
-
-
+#Shows the form that creates a new user
 
 get("/users/new") do
     slim(:"/users/create")
 end
+
+
+#Creates a new user and redirects to / if successful, if the password and the confirmed password do not match the route redirects to /register_error
+#@param [string] :username, the username of the user being created
+#@param [string] :password, the password of the user being created
+#@param [string] :password_confirm, the confirmation of the password of the user being created
+#@param [string] :phone_number, the phone number of the user being created
+#@see Model#register_user
 
 post("/users/new") do
     username = params[:username]
@@ -104,35 +162,17 @@ post("/users/new") do
     password_confirm = params[:password_confirm]
     phone_number = params[:phone_number]
     if password = password_confirm
-        user_id = register_user(username, password, phone_number)
+        array = register_user(username, password, phone_number)
+        session[:user_id] = array[0]
+        session[:group_id] = array[1]
         redirect("/")
     else
         redirect("/register_error")
     end
 end
 
-get("/login") do
-    slim(:login)
-end
 
-post('/login') do
-    username = params[:username]
-    password = params[:password]
-    login_result = login(username, password)
-    if login_result == true
-        redirect('/')
-    else
-        redirect('/login_error')
-    end
-end
-
-get("/login_error") do
-    slim(:login_error)
-end
-
-get("/register_error") do
-    slim(:register_error)
-end
+#Logs out the user
 
 get("/users/logout") do
     session[:user_id] = nil
@@ -140,23 +180,86 @@ get("/users/logout") do
     redirect("/")
 end
 
-get("/users/profile") do
-    if login_check() == false
-        redirect("/")
-    end
-    user_id = session[:user_id].to_i
-    ads_by_user = ads_by_user(user_id)
-    slim(:"/users/profile", locals:{advertisements:ads_by_user})
-end
 
-get("/users/edit") do
+#Shows a list of all the users and allows to delete the users, can only be accessed by admins
+#@see Model#get_user_content
+#@see Model#get_user_to_group
+
+get("/admin/edit") do
     if session[:group_id] != 2
         redirect("/")
     end
     users = get_user_content()
     user_to_group = get_user_to_group()
-    categories = get_categories()
-    byebug
-    slim(:"/admin/users_edit", locals:{users:users, user_to_group:user_to_group, categories:categories})
+    slim(:"/admin/users_edit", locals:{users:users, user_to_group:user_to_group})
 end
+
+
+#Shows a users profile
+#@param [string] :user_id, the id of the user whose profile is being shown
+#@see Model#ads_by_user
+
+get("/users/profile") do
+    user_id = session[:user_id].to_i
+    ads_by_user = ads_by_user(user_id)
+    slim(:"/users/profile", locals:{advertisements:ads_by_user})
+end
+
+
+#Shows the form that allows a user to log in
+
+get("/login") do
+    slim(:login)
+end
+
+
+#Logs a user in, keeps track of the amount of login tries a computer has, if they have more than 3 login_tries they get a cool-down effect, the tracker is reset after the user has been slept for 30 seconds or after a successful login attempt. If the user is successfully logged in the route redirects to /, if the login attempt is unsuccessful but the user does not have more than 3 tries the route redirects to /login, if the login attempt is unsuccessful and the user has more than 3 tries the route will sleep the user for 30 seconds and then redirect to /too_many_login_tries
+#@param [string] :username, the username of the user being logged in
+#@param [string] :password, the password of the user being logged in
+#@see Model#login
+
+post('/login') do
+    if session[:login_tries] == nil
+        session[:login_tries] = 1
+    end
+    username = params[:username]
+    password = params[:password]
+    login_result = login(username, password)
+    session[:user_id] = login_result[0]
+    session[:group_id] = login_result[1]
+    if login_result != nil
+        session[:login_tries] = nil
+        redirect('/')
+    else
+        session[:login_tries] += 1
+        redirect('/error-- /login_error')
+    end
+    if session[:login_tries] > 3
+        redirect("/error/too_many_login_tries")
+    end
+end
+
+
+#
+
+get("/error/too_many_login_tries") do
+    session[:sleep] = true
+    sleep(30)
+    session[:sleep] = false
+    redirect("/login")
+end
+
+get('/error') do
+  slim(:"/error/error")  
+end
+
+get("/error/login_error") do
+    slim(:"/error/login")
+end
+
+get("/register_error") do
+    slim(:"/error/register")
+end
+
+
 
--- model.rb
+++ model.rb
@@ -1,181 +1,161 @@
+module Model
+
+  def add_ad(ad_name, description, db_path, price, category_id, user_id) 
+    if price.include?("kr") == false
+      price = price + " kr"
+    end
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    db.execute("INSERT INTO advertisements(name, description, img_path, price) VALUES (?, ?, ?, ?)", ad_name, description, db_path, price)
+    ad_id_hash = db.execute("SELECT ad_id FROM advertisements WHERE name = ? AND img_path = ?", ad_name, db_path).first
+    ad_id = ad_id_hash["ad_id"].to_i
+    db.execute("INSERT INTO ad_to_user(user_id, ad_id) VALUES (?, ?)", user_id, ad_id)
+    db.execute("INSERT INTO ad_to_category(ad_id, category_id) VALUES (?, ?)", ad_id, category_id)
+  end
+
+  def ads_by_user(user_id)
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT ad_id FROM ad_to_user WHERE user_id = ?",user_id)
+    ads_by_user = result.map do |el|
+      db.execute("SELECT * FROM advertisements WHERE ad_id = ?", el["ad_id"]).first
+    end
+    return ads_by_user
+  end
 
 
+  def get_db_path(id) 
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT img_path FROM advertisements WHERE ad_id = ?", id)
+  end
 
-
-def add_ad(ad_name, description, db_path, price, category_id, user_id) 
-  if price.include?("kr") == false
+  def update_ad(ad_name, description, price, category_id, ad_id)
+    if price.include?("kr") == false
     price = price + " kr"
+    end
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    db.execute("UPDATE advertisements SET name = ?, description = ?, price = ? WHERE ad_id = ?", ad_name, description, price, ad_id)
+    db.execute("UPDATE ad_to_category SET category_id = ? WHERE ad_id = ?", category_id, ad_id)
   end
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  db.execute("INSERT INTO advertisements(name, description, img_path, price) VALUES (?, ?, ?, ?)", ad_name, description, db_path, price)
-  ad_id_hash = db.execute("SELECT ad_id FROM advertisements WHERE name = ? AND img_path = ?", ad_name, db_path).first
-  ad_id = ad_id_hash["ad_id"].to_i
-  db.execute("INSERT INTO ad_to_user(user_id, ad_id) VALUES (?, ?)", user_id, ad_id)
-  db.execute("INSERT INTO ad_to_category(ad_id, category_id) VALUES (?, ?)", ad_id, category_id)
-end
 
-def ads_by_user(user_id)
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  result = db.execute("SELECT ad_id FROM ad_to_user WHERE user_id = ?",user_id)
-  ads_by_user = result.map do |el|
-    db.execute("SELECT * FROM advertisements WHERE ad_id = ?", el["ad_id"]).first
+  def update_ad_picture(db_path, ad_id)
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    db.execute("UPDATE advertisements SET img_path = ? WHERE ad_id = ?", db_path, ad_id)
   end
-  return ads_by_user
-end
 
-def ad_categories()
-
-end
-
-def get_db_path(id) 
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  result = db.execute("SELECT img_path FROM advertisements WHERE ad_id = ?", id)
-end
-
-def update_ad(ad_name, description, price, category_id, ad_id)
-  if price.include?("kr") == false
-    price = price + " kr"
+  def delete_ad(ad_id)
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    db.execute("DELETE FROM advertisements WHERE ad_id = ?", ad_id)
+    db.execute("DELETE FROM ad_to_user WHERE ad_id = ?", ad_id)
+    db.execute("DELETE FROM ad_to_category WHERE ad_id = ?", ad_id)
   end
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  db.execute("UPDATE advertisements SET name = ?, description = ?, price = ? WHERE ad_id = ?", ad_name, description, price, ad_id)
-  db.execute("UPDATE ad_to_category SET category_id = ? WHERE ad_id = ?", category_id, ad_id)
-end
 
-def update_ad_picture(db_path, ad_id)
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  db.execute("UPDATE advertisements SET img_path = ? WHERE ad_id = ?", db_path, ad_id)
-end
-
-def delete_ad(ad_id)
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  db.execute("DELETE FROM advertisements WHERE ad_id = ?", ad_id)
-  db.execute("DELETE FROM ad_to_user WHERE ad_id = ?", ad_id)
-  db.execute("DELETE FROM ad_to_category WHERE ad_id = ?", ad_id)
-end
-
-def ad_content(ad_id)
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  ad_content = db.execute("SELECT * FROM advertisements WHERE ad_id = ?", ad_id).first
-  return ad_content
-end
-
-def ad_to_category(ad_id)
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  category_id = db.execute("SELECT * FROM ad_to_category WHERE ad_id = ?", ad_id).first
-  return category_id
-end
-
-def get_categories()
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  return db.execute("SELECT * FROM categories")
-end
-
-
-
-
-def search_file_ending(original_filename)
-  array = original_filename.split(".")
-  result = "." + array[1]
-  return result
-end
-
-
-
-
-
-
-
-
-def register_user(username, password, phone_number)
-  pwdigest = BCrypt::Password.create(password)
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash
-  db.execute("INSERT INTO users(username, pwdigest, phone_number) VALUES (?, ?, ?)", username, pwdigest, phone_number)
-  result = db.execute("SELECT user_id FROM users WHERE username = ? AND pwdigest = ?", username, pwdigest).first.first
-  user_id = result
-  group_id = 1
-  db.execute("INSERT INTO user_to_group(user_id, group_id) VALUES (?, ?)", user_id, group_id)
-  session[:user_id] = user_id
-  session[:group_id] = group_id
-end
-
-# def get_group_id(username, phone_number)
-#   db = SQLite3::Database.new('db/handel.db')
-#   db.results_as_hash = true
-#   result = db.execute("SELECT group_id FROM user_to_group WHERE username = ? AND phone_number = ?", username, phone_number)
-# end
-
-
-
-def login(username, password)
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-  pwdigest = result["pwdigest"]
-  user_id = result["user_id"]
-  group_id_hash = db.execute("SELECT group_id FROM user_to_group WHERE user_id = ?", user_id).first
-  group_id = group_id_hash["group_id"]
-  if BCrypt::Password.new(pwdigest) == password
-    session[:user_id] = user_id
-    session[:group_id] = group_id
-    return true
-  else
-    return false
+  def ad_content(ad_id)
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    ad_content = db.execute("SELECT * FROM advertisements WHERE ad_id = ?", ad_id).first
+    return ad_content
   end
-end
 
-def locate_user_id(username)
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  id = db.execute("SELECT user_id FROM users WHERE username = ?")
-  session[:user_id] = id
-end
-
-
-
-# def get_user_preferences(user_id)
-#   db = SQLite3::Database.new('db/handel.db')
-#   db.results_as_hash = true
-#   result = db.execute("SELECT category_id FROM users_categories WHERE user_id = ?",user_id)
-#   user_preferences = result.map do |el|
-#     db.execute("SELECT * FROM categories WHERE category_id = ?", el["category_id"]).first
-#   end
-#   return user_preferences
-# end
-
-def get_categories()
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM categories")
-end
-
-def login_check()
-  if session[:user_id] != nil
-    return true
-  else
-    return false
+  def ad_to_category(ad_id)
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    category_id = db.execute("SELECT * FROM ad_to_category WHERE ad_id = ?", ad_id).first
+    return category_id
   end
+
+  def get_categories()
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    return db.execute("SELECT * FROM categories")
+  end
+
+  def search_file_ending(original_filename)
+    array = original_filename.split(".")
+    result = "." + array[1]
+    return result
+  end
+
+  def register_user(username, password, phone_number)
+    pwdigest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash
+    db.execute("INSERT INTO users(username, pwdigest, phone_number) VALUES (?, ?, ?)", username, pwdigest, phone_number)
+    result = db.execute("SELECT user_id FROM users WHERE username = ? AND pwdigest = ?", username, pwdigest).first.first
+    user_id = result
+    group_id = 1
+    db.execute("INSERT INTO user_to_group(user_id, group_id) VALUES (?, ?)", user_id, group_id)
+    return [user_id, group_id]
+  end
+
+  # def get_group_id(username, phone_number)
+  #   db = SQLite3::Database.new('db/handel.db')
+  #   db.results_as_hash = true
+  #   result = db.execute("SELECT group_id FROM user_to_group WHERE username = ? AND phone_number = ?", username, phone_number)
+  # end
+
+  def login(username, password)
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+   result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    if result != nil
+      pwdigest = result["pwdigest"]
+      user_id = result["user_id"]
+      group_id_hash = db.execute("SELECT group_id FROM user_to_group WHERE user_id = ?", user_id).first
+      group_id = group_id_hash["group_id"]    
+      if BCrypt::Password.new(pwdigest) == password
+       return [user_id, group_id]
+      else
+        return nil
+     end
+   else
+      return nil
+    end
+  end
+
+  # def get_user_preferences(user_id)
+  #   db = SQLite3::Database.new('db/handel.db')
+  #   db.results_as_hash = true
+  #   result = db.execute("SELECT category_id FROM users_categories WHERE user_id = ?",user_id)
+  #   user_preferences = result.map do |el|
+  #     db.execute("SELECT * FROM categories WHERE category_id = ?", el["category_id"]).first
+  #   end
+  #   return user_preferences
+  # end
+
+  def get_ads()
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM advertisements")
+    return result
+  end
+
+  def get_user_content()
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    return db.execute("SELECT * FROM users")
+  end
+
+  def get_user_to_group()
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    return db.execute("SELECT * FROM user_to_group")
+  end
+
+  def all_of(*strings)
+    return /(#{strings.join("|")})/
+  end
+ 
+  def check_user_id_ad(ad_id)
+    db = SQLite3::Database.new('db/handel.db')
+    db.results_as_hash = true
+    return db.execute("SELECT user_id FROM ad_to_user WHERE ad_id = ?", ad_id).first
+  end
+
+
+
 end
-
-def get_user_content()
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  return db.execute("SELECT * FROM users")
-end
-
-def get_user_to_group()
-  db = SQLite3::Database.new('db/handel.db')
-  db.results_as_hash = true
-  return db.execute("SELECT * FROM user_to_group")
-end
-
-
